f1038345b5f62f35dcf3f523f1d7937c24175858
NekoMkII <r1burtzlaff@gmail.com>
2016-06-27 22:10:01 -0700

    test animation and test script
    move around with wasd, was just trying to figure out how c# worked
    again, will probably need to figure out controls for android at some
    point...

---
+++ Assets/movement_manual.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement_manual : MonoBehaviour {
+
+    public float speed;
+    public GameObject pet;
+
+	void Start () {
+        speed = 0.1f;
+        transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+    }
+	    
+	void FixedUpdate () {
+        if (Input.GetKey("w")){
+            this.transform.position += new Vector3(0,speed,0);
+        }
+        if (Input.GetKey("s"))
+        {
+            this.transform.position += new Vector3(0, -speed, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            this.transform.position += new Vector3(-speed, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            this.transform.position += new Vector3(speed, 0, 0);
+        }
+
+    }
+}
